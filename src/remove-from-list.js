c14b43d09fc3b8abfbe91b95073327727afaff6f
Evgenia Bychkovskaya <z.bychkovskayaa@gmail.com>
2022-05-03 02:15:44 +0300

    test

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Given a singly linked list of integers l and an integer k,
@@ -22,10 +22,35 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(l, k) {
+  let top = new ListNode();
+  for (let i = l.length - 1; i >= 0; i--) {
+    if (!top.value) {
+      top = new ListNode(l[i]);
+    }
+    else {
+      let nodeTemp = new ListNode(l[i]);
+      nodeTemp.next = top;
+      top = nodeTemp;
+    }
+  }
+  let curr = top;
+  if (top.value == k) {
+    top = top.next;
+    curr = top;
+  }
+  let prev = curr.next;
+  while (curr.next) {
+    if (prev.value == k) {
+      curr.next = curr.next.next;
+    }
+    curr = curr.next;
+    prev = curr.next;
+  }
+  return top;
 }
+
+console.log(removeKFromList([3, 1, 2, 3, 4, 5], 3));
 
 module.exports = {
   removeKFromList
